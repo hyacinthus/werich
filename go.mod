d258e4dfde3d8971434afb8cb5a7a7e2af95038b
Muninn <hyacinthus@gmail.com>
2018-11-27 19:12:02 +0800

    just sync code with errors

--- go.mod
+++ go.mod
@@ -9,4 +9,5 @@ require (
 	github.com/stretchr/testify v1.2.2 // indirect
 	github.com/tj/front v0.0.0-20170212063142-739be213b0a1
 	gopkg.in/yaml.v1 v1.0.0-20140924161607-9f9df34309c0 // indirect
+	gopkg.in/yaml.v2 v2.2.1
 )
--- werich.go
+++ werich.go
@@ -1,10 +1,53 @@
 package werich
 
 import (
+	"bytes"
+
 	"github.com/russross/blackfriday"
+	yaml "gopkg.in/yaml.v2"
 )
 
-// Run convert md to html
-func Run(input []byte) []byte {
-	return blackfriday.Run(input)
+// Delimiter
+var yamldelim = []byte("---")
+
+// bf instance
+var bfhtml, bfrich *blackfriday.Markdown
+
+func init() {
+	bfhtml = blackfriday.New(blackfriday.WithExtensions(blackfriday.CommonExtensions))
+	bfrich = blackfriday.New(blackfriday.WithExtensions(blackfriday.CommonExtensions))
 }
+
+// MD Markdown entity
+type MD struct {
+	// meta data front matter
+	meta []byte
+	// source without front matter
+	body []byte
+	// may be more config
+}
+
+// NewMD make a MD
+func NewMD(src []byte) *MD {
+	var md = new(MD)
+	if bytes.HasPrefix(src, yamldelim) {
+		parts := bytes.SplitN(src, yamldelim, 3)
+		md.meta = parts[1]
+		md.body = parts[2]
+	} else {
+		md.body = src
+	}
+	return md
+}
+
+// Meta unmarshal meta to struct or map v
+func (md *MD) Meta(v interface{}) error {
+	return yaml.Unmarshal(md.meta, v)
+}
+
+// HTML convert md to html
+func (md *MD) HTML() []byte {
+	return bfhtml.Run(md.body)
+}
+
+// Rich render markdown to weapp rich-text json struct
